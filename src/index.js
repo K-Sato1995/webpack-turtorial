9a78bce00467f6bcacadfcff0fcf993709e248ad
Katsuki <Katsuki_Sato0130@icloud.com>
2021-06-25 14:41:53 +0900

    Dynamic importing

--- src/index.js
+++ src/index.js
@@ -1,10 +1,22 @@
 import React, { useState } from "react";
 import { render } from "react-dom";
+import { getUsers } from "./common/usersAPI";
+
+const getUserModule = () => import("./common/usersAPI");
+
+const btn = document.getElementById("btn");
+
+btn.addEventListener("click", () => {
+  getUserModule().then(({ getUsers }) => {
+    getUsers().then(json => console.log(json));
+  });
+});
+
 
 function App() {
-    const [state, setState] = useState("CLICK ME");
+  const [state, setState] = useState("CLICK ME");
 
-    return <button onClick={() => setState("CLICKED")}>{state}</button>;
+  return <button onClick={() => setState("CLICKED")}>{state}</button>;
 }
 
 render(<App />, document.getElementById("root"));
