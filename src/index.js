a517fa063586280e530c8165ba1db94695b368d6
Katsuki <Katsuki_Sato0130@icloud.com>
2022-03-02 12:15:05 +0900

    a

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const btn = document.getElementById("btn");
 btn.addEventListener("click", () => {
   getUserModule().then(({ getUsers }) => {
     getUsers().then(json => console.log(json));
+    
   });
 });
 
